9104f6e3141b7edd4047f20245d791e6e876ecfb
Oke Uguru <okeuguru@users.noreply.github.com>
2017-10-16 20:16:53 -0400

    Update upload.py

--- dynamodb/upload.py
+++ dynamodb/upload.py
@@ -10,16 +10,16 @@ dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
 
 # Print out some data about the table.
 table = dynamodb.Table('ProductCatalog')
-print ("ProductCatalog table contains " + table.item_count + "records")
+print ("ProductCatalog table contains " + str(table.item_count) + "records")
 print
-print '......'s
+print '......'
 print
 print("...Now adding data to ProductCatalog table ...")
 
 with table.batch_writer() as batch:
     batch.put_item(
     Item={
-         'Id': '1101',
+        'Id': 1101,
         'Title': 'Book 101 Title',
         'ISBN': '111-1111111111',
         'Authors': 'Author1',
@@ -33,7 +33,7 @@ with table.batch_writer() as batch:
 
     batch.put_item(
     Item={
-        'Id': '102',
+        'Id': 102,
         'Title': 'Book 102 Title',
         'ISBN': '222-2222222222',
         'Authors': ['Author1', 'Author2'],
@@ -47,7 +47,7 @@ with table.batch_writer() as batch:
 
     batch.put_item(
     Item={
-        'Id': '103',
+        'Id': 103,
         'Title': 'Book 103 Title',
         'ISBN': '333-3333333333',
         'Authors': ['Author1', 'Author2'],
@@ -61,7 +61,7 @@ with table.batch_writer() as batch:
 
     batch.put_item(
     Item={
-        'Id': '201',
+        'Id': 201,
         'Title': '18-Bike-201',
         'Description': '201 Description',
         'BicycleType': 'Road',
@@ -75,7 +75,7 @@ with table.batch_writer() as batch:
 
     batch.put_item(
     Item={
-        'Id': '202',
+        'Id': 202,
         'Title': '21-Bike-202',
         'Description': '202 Description',
         'BicycleType': 'Road',
@@ -89,12 +89,12 @@ with table.batch_writer() as batch:
 
     batch.put_item(
     Item={
-        'Id': '203',
+        'Id': 203,
         'Title': '19-Bike-203',
         'Description': '203 Description',
         'BicycleType': 'Road',
         'Brand': 'Brand-Company B',
-        'Price'; '300',
+        'Price': '300',
         'Gender': 'W',
         'Color': ['Red', 'Green', 'Black'],
         'ProductCategory': 'Bicycle'
@@ -103,7 +103,7 @@ with table.batch_writer() as batch:
 
     batch.put_item(
     Item={
-        'Id': '204',
+        'Id': 204,
         'Title': '18-Bike-204',
         'Description': '204 Description',
         'BicycleType': 'Mountain',
@@ -117,7 +117,7 @@ with table.batch_writer() as batch:
 
     batch.put_item(
     Item={
-        'Id': '205',
+        'Id': 205,
         'Title': '20-Bike-205',
         'Description': '205 Description',
         'BicycleType': 'Hybrid',
@@ -132,7 +132,7 @@ with table.batch_writer() as batch:
 
 # Print out some data about the table.
 table = dynamodb.Table('Forum')
-print("Forum table contains " + table.item_count +"records")
+print("Forum table contains " + str(table.item_count) +"records")
 print
 print '......'
 print
@@ -142,7 +142,7 @@ with table.batch_writer() as batch:
     batch.put_item(
     Item={
         'Name': 'Amazon DynamoDB',
-        'Category': 'Amazon Web Services'),
+        'Category': 'Amazon Web Services',
         'Threads': '0',
         'Messages': '0',
         'Views': '1000'
@@ -162,7 +162,7 @@ with table.batch_writer() as batch:
 # Print out some data about the table.
 
 table = dynamodb.Table('Reply')
-print("Reply table contains " + table.item_count + "records")
+print("Reply table contains " + str(table.item_count) + "records")
 print
 print '......'
 print
@@ -174,7 +174,7 @@ with table.batch_writer() as batch:
     batch.put_item(
     Item={
         'Id': 'Amazon DynamoDB#DynamoDB Thread 1',
-        'ReplyDateTime': date_now - timedelta(days=14),
+        'ReplyDateTime': str(date_now - timedelta(days=14)),
         'Message': 'DynamoDB Thread 1 Reply 2 text',
         'PostedBy': 'User B'
         }
@@ -183,7 +183,7 @@ with table.batch_writer() as batch:
     batch.put_item(
     Item={
         'Id': 'Amazon DynamoDB#DynamoDB Thread 2',
-        'ReplyDateTime': date_now - timedelta(days=21),
+        'ReplyDateTime': str(date_now - timedelta(days=21)),
         'Message': 'DynamoDB Thread 2 Reply 3 text',
         'PostedBy': 'User B'
         }
@@ -192,7 +192,7 @@ with table.batch_writer() as batch:
     batch.put_item(
     Item={
         'Id': 'Amazon DynamoDB#DynamoDB Thread 2',
-        'ReplyDateTime': date_now - timedelta(days=7),
+        'ReplyDateTime': str(date_now - timedelta(days=7)),
         'Message': 'DynamoDB Thread 2 Reply 2 text',
         'PostedBy': 'User A'
         }
@@ -201,7 +201,7 @@ with table.batch_writer() as batch:
     batch.put_item(
     Item={
         'Id': 'Amazon DynamoDB#DynamoDB Thread 2',
-        'ReplyDateTime': date_now - timedelta(days=1),
+        'ReplyDateTime': str(date_now - timedelta(days=1)),
         'Message': 'DynamoDB Thread 2 Reply 1 text',
         'PostedBy': 'User A'
         }
